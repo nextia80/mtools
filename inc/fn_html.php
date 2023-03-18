27b25d78621b31c2eebe86a7906295c11b45f5fc
nextia80 <nextia80@gmail.com>
2023-03-18 18:13:10 +0900

    add

---
+++ inc/fn_html.php
@@ -0,0 +1,12 @@
+<?php
+/**
+	@class  : fn_html.php
+	@doc	: html function collaction
+	@auther : mongE
+	@version : 1.0, 2023.03.18, create
+
+*/
+
+
+
+?>
---
+++ main.php
@@ -0,0 +1,23 @@
+<?PHP
+/**
+	@class  : index.php
+	@doc	: index file
+	@auther : mongE
+	@version : 1.0, 2023.03.14, create
+*/
+include "./inc/dbconn.inc"; # 디비정보
+include "./inc/siteinfo.inc"; # 기본세팅
+include "./inc/fn_function.php"; # 공통 html funciton
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
