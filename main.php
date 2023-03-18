06bce14fabc346890c76a89946e39c0f5c405f18
nextia80 <nextia80@gmail.com>
2023-03-19 08:10:52 +0900

    midify

--- main.php
+++ main.php
@@ -15,7 +15,7 @@ include "./inc/fn_function.php"; # 공통 html funciton
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?=$_title?></title>
 </head>
 <body>
     
