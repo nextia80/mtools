c939000ba9296cc5b9220792be1c321184e60dc8
nextia80 <nextia80@gmail.com>
2023-04-07 08:09:43 +0900

    20230407

--- main.php
+++ main.php
@@ -1,15 +1,19 @@
 <?php
+
     /** markdown ************************************************************************/
+    /*
+    markdown Ex
     spl_autoload_register(function($class){
         require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
     });
     use Michelf\Markdown;
+    */    
+    //$text = file_get_contents('README.md');
+    //$html = Markdown::defaultTransform($text);
+    //echo  $html;
     /** markdown ************************************************************************/
-    $text = file_get_contents('README.md');
-    $html = Markdown::defaultTransform($text);
-
+    
     $_body_tag = array("topmargin" => "0", "leftmargin" => "0" );
     fn_html_body($_body_tag);
-	echo $html;
 ?>
 welcom to mTools
