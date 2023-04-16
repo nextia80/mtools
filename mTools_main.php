8cdb09bffc54f148cf66adbe79f037fc4e7e12f2
nextia80 <nextia80@gmail.com>
2023-04-17 06:17:05 +0900

    2023 04 17

--- mTools_main.php
+++ mTools_main.php
@@ -2,9 +2,12 @@
 $_body_tag = array("topmargin" => "0", "leftmargin" => "0" );
 fn_body($_body_tag);
 ?>
+<h1>MJCT</h1> 
+<? echo "접속시간 : ". date("Y-m-d H:i:s")."<br/>"; ?>
 <br>
 <br>hi2!
 <br>
 <br>git 초기 설정 
 <br>git config --global user.name "nextia80"
 <br>git config --global user.email "nextia80@gmail.com"
+<br>
