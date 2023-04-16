aabdf7fda6ecdd06d8211f3165fb0933674f5be3
nextia80 <nextia80@gmail.com>
2023-04-16 16:36:06 +0900

    2023 04 16

--- mTools_main.php
+++ mTools_main.php
@@ -2,6 +2,9 @@
 $_body_tag = array("topmargin" => "0", "leftmargin" => "0" );
 fn_body($_body_tag);
 ?>
-
-
-hi!
+<br>
+<br>hi2!
+<br>
+<br>git 초기 설정 
+<br>git config --global user.name "nextia80"
+<br>git config --global user.email "nextia80@gmail.com"
