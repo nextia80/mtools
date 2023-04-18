e28ef57a5f0e154b07440a355cb7ebcb11e375bd
nextia80 <nextia80@gmail.com>
2023-04-18 14:19:15 +0900

    2023 04 18

--- mTools_main.php
+++ mTools_main.php
@@ -1,24 +1,19 @@
 <?PHP
-$_body_tag = array("topmargin" => "0", "leftmargin" => "0" );
+$_body_tag = array("topmargin" => "0", "leftmargin" => "10" );
 fn_body($_body_tag);
 ?>
 <h1>MJCT</h1> 
 <? echo "접속시간 : ". date("Y-m-d H:i:s")."<br/>"; ?>
 <br>
-<br>hi2!
-<br>
-<br>git 초기 설정 
+    <h2>git 초기 설정</h2>
 <br>git config --global user.name "nextia80"
 <br>git config --global user.email "nextia80@gmail.com"
 <br>
-<br>
-
-1. DB 설정
-2. Create / Drop
-3. select 
-4. insert 
-5. update
-6. delete
-7. session
-
-8. script 1 th'
+<br>1. DB 설정
+<br>2. Create / Drop
+<br>3. select 
+<br>4. insert 
+<br>5. update
+<br>6. delete
+<br>7. session
+<br>8. script 1 th'
